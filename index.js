bbba4bafcde5abfe689e24a4cbaf09f473bfe24d
reyhannnn1502 <reyhansatriab@gmail.com>
2025-05-14 14:55:31 +0700

    save

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.get("/", (req,res)=>{
+    res.send("Hello World");
+}
+);
 app.use("/users", require("./routes/userRoute"));
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
